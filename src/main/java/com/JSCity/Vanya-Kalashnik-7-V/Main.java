43b582dd8fd108c6b310afc9beca5a2604938b2f
Vanyak25 <38924786+Vanyak25@users.noreply.github.com>
2018-05-08 01:33:37 +0300

    Update Main.java

--- src/main/java/com/JSCity/Vanya-Kalashnik-7-V/Main.java
+++ src/main/java/com/JSCity/Vanya-Kalashnik-7-V/Main.java
@@ -6,8 +6,8 @@ import eu.printingin3d.javascad.utils.SaveScadFiles;
 
 public class Main {
     public static void main(String[] args) throws IllegalValueException, IOException {
-        new SaveScadFiles(new File("D:\Vanya\Laba_5\IdeaProjects")).
-                addModel("parus.scad", new Building()).
+        new SaveScadFiles(new File("D:\Vanya\Laba_5")).
+                addModel("Kaskad.scad", new Building()).
                 saveScadFiles();
 
     }
